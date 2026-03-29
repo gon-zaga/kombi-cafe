058ab296c638fef808b5e087f1ce6c9b8a5df076
Justin Gonzaga <justingonzaga333@gmail.com>
2026-03-29 21:13:24 +0800

    Added Categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
+'use client'
 import './globals.css'
-import Header from '@/app/components/Header';
-import Promotions from '@/app/components/Promotions';
+import Header from '@/app/ui/Header';
+import Promotions from '@/app/ui/Promotions';
+import CategoryBar from './ui/CategoryBar';
+import React, {useState} from 'react';
 export default function Home() {
+  const [active, setActive] = useState("Kaffee");
   return (
     <div className='bg-cream min-h-screen'>
       <Header />
       <h2 className='font-roboto-slab text-2xl text-center mb-1 flex items-center justify-center'> MENU</h2>
       <Promotions />
+      <CategoryBar active={active} setActive={setActive} />
+      <div className="h-12 flex justify-center items-center">
+        {active}
+      </div>
+      <hr />
     </div>
     );
 }
---
+++ app/ui/CategoryBar.tsx
@@ -0,0 +1,62 @@
+'use client'
+import Image from "next/image";
+
+function CategoryBar({active, setActive}: {active: string, setActive: (category: string) => void}) {
+  const categories = [
+    "Kaffee",
+    "Klassik Kaffee",
+    "Specialty Kaffee",
+    "Kaffee Frappe",
+    "Non-Kaffe Latte",
+    "Fruitte Latte",
+    "The Beetles Juice", 
+    "Non-Kaffee Frappe"
+  ];
+
+  return(
+    <>
+      <section className="text-dark-brown">
+        <hr className="border-dark-brown"/>
+        <div className="w-full flex items-center">
+          <section className="flex flex-row p-1 gap-0.5 shrink-0">
+            <Image 
+              src="/search-icon.svg"
+              alt="search-bar"
+              width={32}
+              height={32}
+            />
+            <Image
+              src="/bullet-list.svg"
+              alt="bullet-list-icon"
+              width={32}
+              height={32}
+            />
+          </section>
+          {/*VERTICAL DIVIDER*/}
+          <div className="h-10 flex border-l border-dark-brown mx-2.5 shrink-0"></div>
+          <section className="flex overflow-x-auto scrollbar-hide">
+            <ul className="flex flex-row gap-3"> 
+              {categories.map((category) => (
+                <li key={category}>
+                  <button
+                    onClick={() => setActive(category)}
+                    className={`whitespace-nowrap pb-1 border-b-2 transition-all
+                      ${active === category
+                        ? "border-dark-brown font-bold"
+                        : "border-transparent"
+                      }`}
+                  >
+                    {category}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </section>
+        </div>
+        <hr className="border-dark-brown"/>
+      </section>
+  </>
+  );
+}
+
+export default CategoryBar;
